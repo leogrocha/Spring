c3c3d75ad2d69dcd851debf148a26686920cce5b
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-05-06 09:13:11 -0300

    Iniciando alura-eventos - Olá Mundo

---
+++ alura-eventos/src/main/java/com/alura/aluraeventos/AluraEventosApplication.java
@@ -0,0 +1,13 @@
+package com.alura.aluraeventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraEventosApplication.class, args);
+	}
+
+}
---
+++ alura-eventos/src/test/java/com/alura/aluraeventos/AluraEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.aluraeventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
