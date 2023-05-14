55e6f9fe97c4b9c198d8268e87e019f29864aa65
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-05-14 12:55:01 -0300

    Criando Estrutura do Projeto e adicionando lista de games

---
+++ dslist/src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -0,0 +1,11 @@
+package com.devsuperior.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+    // Foi informado o tipo Long pois foi o tipo da variável ID do objeto Game
+    
+
+}
