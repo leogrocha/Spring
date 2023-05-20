4b7994f521d5a17edc1f77bf8df8a8637210a5ea
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-05-20 09:02:41 -0300

    Homologação OK

--- dslist/src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ dslist/src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -12,7 +12,7 @@ public interface GameRepository extends JpaRepository<Game, Long> {
     // Foi informado o tipo Long pois foi o tipo da variável ID do objeto Game
 
     @Query(nativeQuery = true, value = """
-		SELECT tb_game.id, tb_game.title, tb_game.game_year AS `year`, tb_game.img_url AS imgUrl,
+		SELECT tb_game.id, tb_game.title, tb_game.game_year AS gameYear, tb_game.img_url AS imgUrl,
 		tb_game.short_description AS shortDescription, tb_belonging.position
 		FROM tb_game
 		INNER JOIN tb_belonging ON tb_game.id = tb_belonging.game_id
