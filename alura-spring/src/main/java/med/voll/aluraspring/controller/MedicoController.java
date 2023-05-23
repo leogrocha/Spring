b4449bf17fbb90a40dcde365fc2bba51c9ac327a
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-05-22 21:29:30 -0300

    Adicionando validações com Bean Validation e Migrations para salvar no banco de dados

--- alura-spring/src/main/java/med/voll/aluraspring/controller/MedicoController.java
+++ alura-spring/src/main/java/med/voll/aluraspring/controller/MedicoController.java
@@ -1,19 +1,28 @@
 package med.voll.aluraspring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import med.voll.aluraspring.medico.DadosCadastroMedico;
+import med.voll.aluraspring.medico.Medico;
+import med.voll.aluraspring.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("medicos")
 public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository;
     
     @PostMapping
-    public void cadastar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+    @Transactional
+    public void cadastar(@RequestBody @Valid DadosCadastroMedico dados) {
+        repository.save(new Medico(dados));
     }
 
 
